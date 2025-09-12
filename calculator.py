2f66a9fc4d561477cb5c3949a963c21547190139
timhoolihan <timhoolihan96@gmail.com>
2025-09-12 01:51:40 +0000

    Added functions Added warnings for division by zero Added support for 'x' as multiplication operator Added variable to store answer and print it at the end in a string

--- calculator.py
+++ calculator.py
@@ -1,35 +1,34 @@
-check=True
-while check==True:
-    num1=input("Enter first number: ")
-    try:
-        float(num1)
-        check=False
-    except:
-        print("Try again")
+def number_input(prompt):
+    while True:
+        value = input(prompt)
+        try:
+            return float(value)
+        except:
+            print("Try again")
 
-check=True
-while check==True:
-    operator=input("Enter operator: ")
-    if operator in ("+", "-", "/", "*"):
-        check=False
+num1 = number_input("Enter first number: ")
+
+while True:
+    operator = input("Enter operator: ")
+    if operator in ("+" "-", "/", "*", "x"):
+        break
     else:
         print("Try again")
         
-check=True
-while check==True:
-    num2=input("Enter second number: ")
-    try: 
-        float(num2)
-        check=False
-    except:
-        print("Try again")
-        
+num2 = number_input("Enter second number: ")   
+
 match operator:
     case "+":
-        print(float(num1)+float(num2))
+        answer = (float(num1) + float(num2))
     case "-":
-        print(float(num1)-float(num2))
+        answer = (float(num1) - float(num2))
     case "/":
-        print(float(num1)/float(num2))
-    case "*":
-        print(float(num1)*float(num2))
+        if float(num2) == 0:
+            print("Error: Division by zero is not allowed.")
+        else:
+            answer = (float(num1) / float(num2))
+    case "*" | "x":
+        answer = (float(num1) * float(num2))
+        operator = "x"
+
+print(f"{num1} {operator} {num2}" + " is " + f"{answer}")  
